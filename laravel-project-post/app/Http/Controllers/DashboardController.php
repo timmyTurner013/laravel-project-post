51f52bafb2ebfd6477f04a2223368cd6e059cd9e
LAPTOP-BB7MGH5V\Lenovo <mazedur@sagbrain.com>
2022-08-15 22:03:57 +0600

    completed

---
+++ laravel-project-post/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\PostLiked;
+
+class DashboardController extends Controller
+{
+    public function __construct() {
+        $this->middleware(['auth']);
+    }
+    public function index() {
+        return view('dashboard');
+    }
+}
